752a0232ef91f682f9cd021c7349c2adb610b65e
pfropfen <steinhauer@live.de>
2025-05-02 14:15:54 +0200

    ca

--- rebirth.sh
+++ rebirth.sh
@@ -1,5 +1,6 @@
 #!bin/bash
-sudo docker container stop $(sudo docker ps -a) && sudo docker container rm $(sudo docker ps -a)
+sudo docker container stop $(sudo docker ps -a) 
+     && sudo docker container rm $(sudo docker ps -a)
 kubectl delete -f wfcdeploy.yaml
 kubectl delete --all pods --grace-period=0 --force
 sudo docker image rm -f $(sudo docker image ls)
